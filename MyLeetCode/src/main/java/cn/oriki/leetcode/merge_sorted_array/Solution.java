f1e57304fe16579e6e2475f77fde632b69a0c2ad
oriki <n0rk1n@live.cn>
2025-02-19 11:49:05 +0800

    88. 合并两个有序数组 ✅

---
+++ MyLeetCode/src/main/java/cn/oriki/leetcode/merge_sorted_array/Solution.java
@@ -0,0 +1,23 @@
+package cn.oriki.leetcode.merge_sorted_array;
+
+import java.util.Arrays;
+
+/**
+ * {@code @site: <a href="https://leetcode.cn/problems/merge-sorted-array/">88. 合并两个有序数组</a>}
+ * {@code @rating: ⭐️}
+ * {@code @tags: 数组, 双指针, 排序}
+ */
+public class Solution {
+
+    /**
+     * 采用库函数的方式解决
+     */
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (n >= 0)
+            System.arraycopy(nums2, 0, nums1, m, n);
+        Arrays.sort(nums1);
+    }
+}
+
+// 执行用时分布 1ms，击败 29.98%
+// 消耗内存分布 41.61MB，击败 7.86%
