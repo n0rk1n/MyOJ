2453f92c9e632e268ebecf3638b458cf436b3d41
oriki <n0rk1n@live.cn>
2025-02-19 12:15:42 +0800

    0. add Class: QuickStart

---
+++ MyLeetCode/src/main/java/cn/oriki/leetcode/QuickStart.java
@@ -0,0 +1,11 @@
+package cn.oriki.leetcode;
+
+public class QuickStart {
+
+    /**
+     * {@code @site: https://leetcode.cn/problemset/}
+     */
+    public static void main(String[] args) {
+
+    }
+}
