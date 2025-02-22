35682870cc5c862912461f7ebecd60805deead61
oriki <n0rk1n@live.cn>
2025-02-22 10:38:27 +0800

    119. 杨辉三角 II ✅

---
+++ MyLeetCode/src/main/java/cn/oriki/leetcode/pascals_triangle_ii/Solution.java
@@ -0,0 +1,34 @@
+package cn.oriki.leetcode.pascals_triangle_ii;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * {@code @site: <a href="https://leetcode.cn/problems/pascals-triangle-ii/">119. 杨辉三角 II</a>}
+ * {@code @rating: ⭐️⭐️}
+ * {@code @tags: 数组, 动态规划}
+ */
+public class Solution {
+
+    /**
+     * 对 118. 杨辉三角 的修改
+     */
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> C = new ArrayList<>();
+        for (int i = 0; i <= rowIndex; ++i) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; ++j) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(C.get(i - 1).get(j - 1) + C.get(i - 1).get(j));
+                }
+            }
+            C.add(row);
+        }
+        return C.get(rowIndex);
+    }
+}
+
+// 执行用时分布 2ms，击败 10.54%
+// 消耗内存分布 40.34MB，击败 63.19%
