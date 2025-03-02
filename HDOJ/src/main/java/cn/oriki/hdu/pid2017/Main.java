14a91ab3c4a171c5ab226db3e5f078585bfb6f0f
oriki <n0rk1n@live.cn>
2025-03-02 21:21:34 +0800

    HDU 2017. 字符串统计 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2017/Main.java
@@ -0,0 +1,21 @@
+package cn.oriki.hdu.pid2017;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        while (n-- != 0) {
+            char[] chars = in.next().toCharArray();
+            int sum = 0;
+            for (char c : chars) {
+                if (Character.isDigit(c)) {
+                    sum++;
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+}
