bfb35d59d89e5b816451d5109c516471a26a744c
oriki <n0rk1n@live.cn>
2025-03-02 16:33:09 +0800

    HDU 2011. 多项式求和 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2011/Main.java
@@ -0,0 +1,19 @@
+package cn.oriki.hdu.pid2011;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int fistLine = in.nextInt(); // 获取第一行数据，也就是一共要执行几次多项式求和方法
+        while (fistLine-- > 0) {
+            int n = in.nextInt();
+            double sum = 0;
+            for (int i = 1; i <= n; i++) {
+                sum += Math.pow(-1, i - 1) / i;
+            }
+            System.out.printf("%.2f\n", sum);
+        }
+    }
+}
