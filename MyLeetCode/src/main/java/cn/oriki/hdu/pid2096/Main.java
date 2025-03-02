9db8155d4257e1c00347b31d65e14400aaebd9d0
oriki <n0rk1n@live.cn>
2025-03-02 11:13:52 +0800

    HDU 2096. 小明 A + B ✅

---
+++ MyLeetCode/src/main/java/cn/oriki/hdu/pid2096/Main.java
@@ -0,0 +1,18 @@
+package cn.oriki.hdu.pid2096;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+
+            int c = ((a % 100) + (b % 100)) % 100;
+            System.out.println(c);
+        }
+    }
+}
