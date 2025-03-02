689e7874cb05e3b4713f61c6b27ff1dad29a2de0
oriki <n0rk1n@live.cn>
2025-03-02 15:33:58 +0800

    HDU 2009. 求数列的和 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2009/Main.java
@@ -0,0 +1,28 @@
+package cn.oriki.hdu.pid2009;
+
+import java.util.Scanner;
+
+public class Main {
+
+    /**
+     * Sample Input:
+     * 81 4
+     * <p>
+     * 意思是第一个数是 81，第二项是 9，第三项是 3，第四项是 根 3
+     * 那么和为 81 + 9 + 3 + 根 3 = 94.x
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            int n = in.nextInt();
+            int m = in.nextInt();
+            double sum = 0;
+            double x = n;
+            for (int i = 0; i < m; i++) {
+                sum += x;
+                x = Math.sqrt(x);
+            }
+            System.out.printf("%.2f\n", sum);
+        }
+    }
+}
