1099d1bf5b15067cba7e047566c4aa92a14ef344
oriki <n0rk1n@live.cn>
2025-03-02 21:09:10 +0800

    HDU 2016. 数据的交换输出 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2016/Main.java
@@ -0,0 +1,33 @@
+package cn.oriki.hdu.pid2016;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            int m = in.nextInt();
+            if (m == 0) {
+                break;
+            }
+            int[] x = new int[m];
+            int min = Integer.MAX_VALUE;
+            int p = 0;
+            for (int i = 0; i < m; i++) {
+                x[i] = in.nextInt();
+                if (x[i] < min) {
+                    min = x[i];
+                    p = i;
+                }
+            }
+            int temp = x[0];
+            x[0] = x[p];
+            x[p] = temp;
+            for (int i = 0; i < m - 1; i++) {
+                System.out.print(x[i] + " ");
+            }
+            System.out.println(x[m - 1]);
+        }
+    }
+}
