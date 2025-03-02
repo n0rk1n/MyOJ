f846278b5e4bbb0bf04408cb0bd8e7c10e0433d2
oriki <n0rk1n@live.cn>
2025-03-02 12:36:10 +0800

    HDU 2002. 计算球体积 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2002/Main.java
@@ -0,0 +1,18 @@
+package cn.oriki.hdu.pid2002;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static final double PI = 3.1415927;
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextDouble()) {
+            double r = in.nextDouble();
+            // double S = 4 * PI * r * r * r / 3;
+            double S = 4 * PI * Math.pow(r, 3) / 3;
+            System.out.printf("%.3f\n", S);
+        }
+    }
+}
