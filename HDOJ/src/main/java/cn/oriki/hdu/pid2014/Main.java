dca1c78a34518f89cf09d7e18ef8835b84de2d13
oriki <n0rk1n@live.cn>
2025-03-02 21:01:22 +0800

    HDU 2014. 青年歌手大奖赛_评委会打分 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2014/Main.java
@@ -0,0 +1,28 @@
+package cn.oriki.hdu.pid2014;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            int n = in.nextInt();
+            int[] s = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                int a = in.nextInt();
+                s[i] = a;
+            }
+
+            int sum = 0;
+            Arrays.sort(s);
+            for (int i = 1; i < n - 1; i++) {
+                sum += s[i];
+            }
+            double avg = sum * 1.0 / (n - 2);
+            System.out.printf("%.2f\n", avg);
+        }
+    }
+}
