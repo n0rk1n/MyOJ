0bfe57f0598469222797a7881d6aeaa0578a1fd4
oriki <n0rk1n@live.cn>
2025-03-02 15:57:58 +0800

    HDU 2010. 水仙花数 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2010/Main.java
@@ -0,0 +1,36 @@
+package cn.oriki.hdu.pid2010;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            int start = in.nextInt();
+            int end = in.nextInt();
+
+            List<Integer> s = new ArrayList<>();
+            int x, y, z;
+            for (int i = start; i <= end; i++) {
+                x = i / 100;
+                y = i / 10 - x * 10;
+                z = i - y * 10 - x * 100;
+                if (x * x * x + y * y * y + z * z * z == i) {
+                    s.add(i);
+                }
+            }
+
+            if (s.isEmpty()) {
+                System.out.println("no");
+            } else {
+                for (int i = 0; i < s.size(); i++) {
+                    System.out.print(s.get(i));
+                    if (i != s.size() - 1) {
+                        System.out.print(" ");
+                    }
+                }
+            }
+        }
+    }
+}
