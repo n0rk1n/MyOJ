2a741e7b6655cdd78f0fb9993f994df7196b6dba
oriki <n0rk1n@live.cn>
2025-03-02 09:55:00 +0800

    HDU 1000. A + B Problem ✅

---
+++ MyLeetCode/src/main/java/cn/oriki/hdu/pid1000/Main.java
@@ -0,0 +1,16 @@
+package cn.oriki.hdu.pid1000;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            int a = scanner.nextInt();
+            int b = scanner.nextInt();
+            System.out.println(a + b);
+        }
+        scanner.close();
+    }
+}
