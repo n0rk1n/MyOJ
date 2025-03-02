e40150c09552301ea703e8ac49319e98b874b13e
oriki <n0rk1n@live.cn>
2025-03-02 15:39:20 +0800

    HDU 2070. Fibbonacci Number ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2070/Main.java
@@ -0,0 +1,32 @@
+package cn.oriki.hdu.pid2070;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static long[] f = new long[51];
+
+    static{
+        fibbonacci();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        while (in.hasNextInt()) {
+            int n = in.nextInt();
+            if (n == -1) {
+                break;
+            }
+            System.out.println(f[n]);
+        }
+    }
+
+    private static void fibbonacci() {
+        f[0] = 0;
+        f[1] = 1;
+        for (int i = 2; i < 51; i++) {
+            f[i] = f[i - 1] + f[i - 2];
+        }
+    }
+}
