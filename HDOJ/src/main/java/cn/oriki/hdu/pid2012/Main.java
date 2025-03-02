3cc3c08ab9a2a7302c3ad7c862c2841e8b7f823e
oriki <n0rk1n@live.cn>
2025-03-02 16:44:26 +0800

    HDU 2012. 素数判定 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2012/Main.java
@@ -0,0 +1,47 @@
+package cn.oriki.hdu.pid2012;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            int start = in.nextInt();
+            int end = in.nextInt();
+            if (start == 0 && end == 0) {
+                break;
+            }
+
+            boolean flag = true;
+            for (int i = start; i <= end; i++) {
+                if (!isPrime(f(i))) {
+                    flag = false;
+                    break;
+                }
+            }
+
+            System.out.println(flag ? "OK" : "Sorry");
+        }
+    }
+
+    public static int f(int n) {
+        return (n * n) + n + 41;
+    }
+
+    /**
+     * 判定素数的高效方法
+     *
+     * @param n 待判定数据
+     * @return 是否为素数 true 为素数、false 不是素数
+     */
+    public static boolean isPrime(int n) {
+        // 从 2 开始判定即可
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) { // 这里是取余
+                return false;
+            }
+        }
+        return true;
+    }
+}
