242b5b29443c7d9bd2b2bf576c0a1b01090aa2a5
oriki <n0rk1n@live.cn>
2025-03-03 09:04:18 +0800

    HDU 2018. 母牛的故事 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2018/Main.java
@@ -0,0 +1,30 @@
+package cn.oriki.hdu.pid2018;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            if (n != 0) {
+                List<Integer> list = new ArrayList<>(8);
+                list.add(0, 0);
+                list.add(1, 1);
+                list.add(2, 2);
+                list.add(3, 3);
+                list.add(4, 4);
+                for (int i = 5; i <= n; i++) {
+                    list.add(i, list.get(i - 1) + list.get(i - 3));
+                }
+                System.out.println(list.get(n));
+            } else {
+                break;
+            }
+        }
+        scanner.close();
+    }
+}
