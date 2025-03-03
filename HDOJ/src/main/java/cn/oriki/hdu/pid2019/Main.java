b03ce15aa8b2ae8a0567423fe4322676a59ac2af
oriki <n0rk1n@live.cn>
2025-03-03 09:14:50 +0800

    HDU 2019. 数列有序! ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2019/Main.java
@@ -0,0 +1,29 @@
+package cn.oriki.hdu.pid2019;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            int m = scanner.nextInt();
+            if (n == 0 && m == 0) {
+                break;
+            }
+            int[] x = new int[n + 1];
+            for (int i = 0; i < n; i++) {
+                x[i] = scanner.nextInt();
+            }
+            x[n] = m;
+            Arrays.sort(x); // 这个方法有点作弊了
+            for (int i = 0; i < n; i++) {
+                System.out.print(x[i] + " ");
+            }
+            System.out.println(x[n]);
+        }
+        scanner.close();
+    }
+}
