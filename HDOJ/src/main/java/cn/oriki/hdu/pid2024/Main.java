70f7b6a4f51f8683fad82d2baae2aefb2ebe14b4
oriki <n0rk1n@live.cn>
2025-03-16 09:53:21 +0800

    HDU 2024. C 语言合法标识符 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2024/Main.java
@@ -0,0 +1,34 @@
+package cn.oriki.hdu.pid2024;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+        while (n-- > 0) {
+            String s = in.nextLine();
+            char[] chars = s.toCharArray();
+            boolean flag = true;
+            if (chars[0] == '_' || Character.isLetter(chars[0])) {
+                for (int i = 1; i < chars.length; i++) {
+                    if (Character.isLetterOrDigit(chars[i]) || chars[i] == '_') {
+                        continue;
+                    }
+                    flag = false;
+                    break;
+                }
+            } else {
+                System.out.println("no");
+                continue;
+            }
+            if (flag) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+}
