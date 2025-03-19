6735edcd56b835d04dc9adfbc371c2f40849e999
oriki <n0rk1n@live.cn>
2025-03-19 21:18:32 +0800

    HDU 2030. 汉字统计 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2030/Main.java
@@ -0,0 +1,22 @@
+package cn.oriki.hdu.pid2030;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        in.nextLine();
+        while (n-- > 0) {
+            String s = in.nextLine();
+            byte[] bytes = s.getBytes();
+            int count = 0;
+            for (byte b : bytes) {
+                if (b < 0)
+                    count++;
+            }
+            System.out.println(count / 2);
+        }
+    }
+}
