4da9709cd8b5f50dec44592a2c6f680d22067fd5
oriki <n0rk1n@live.cn>
2025-03-19 23:19:50 +0800

    HDU 2039. 三角形 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2039/Main.java
@@ -0,0 +1,22 @@
+package cn.oriki.hdu.pid2039;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        while (m-- > 0) {
+            double a = in.nextDouble();
+            double b = in.nextDouble();
+            double c = in.nextDouble();
+
+            if ((a + b > c) && (a + c > b) && (c + b > a)) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
