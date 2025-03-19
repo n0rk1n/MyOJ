ab3086f69427ec8adc8c4be360f0b26a5b49c154
oriki <n0rk1n@live.cn>
2025-03-19 19:54:26 +0800

    HDU 2025. 查找最大元素 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2025/Main.java
@@ -0,0 +1,30 @@
+package cn.oriki.hdu.pid2025;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String string = in.nextLine();
+            // 字母全部为小写
+            char[] s = string.toCharArray();
+            char min = 'a';
+            for (char c : s) {
+                if (c > min) {
+                    min = c;
+                }
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+            for (char c : s) {
+                if (c == min) {
+                    stringBuilder.append(c).append("(max)");
+                } else {
+                    stringBuilder.append(c);
+                }
+            }
+            System.out.println(stringBuilder);
+        }
+    }
+}
