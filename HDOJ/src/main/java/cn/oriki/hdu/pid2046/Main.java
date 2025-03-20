9c355ab14f26a0a841a06152de0d722ae6a056bb
oriki <n0rk1n@live.cn>
2025-03-20 10:47:16 +0800

    HDU 2046. 骨牌铺方格 ✅️

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2046/Main.java
@@ -0,0 +1,21 @@
+package cn.oriki.hdu.pid2046;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            int t = in.nextInt();
+            long[] x = new long[51];
+            x[0] = 1; // 2 * (0 + 1) = 2 的格子，2 个盒子有两种放法
+            x[1] = 2; // 2 * (1 + 1) = 4 的格子，4 个盒子有两种放法
+            x[2] = 3; // 2 * (2 + 1) = 6 的格子，6 个格子有三种放法
+            for (int i = 3; i < 51; i++) {
+                x[i] = x[i - 2] + x[i - 1];
+            }
+            System.out.println(x[t - 1]);
+        }
+    }
+}
