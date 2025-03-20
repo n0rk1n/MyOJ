9987d2ee2b37d0fddfbdde0c0ee8eb50397fd692
oriki <n0rk1n@live.cn>
2025-03-20 10:20:18 +0800

    HDU 2042. 不容易系列之二 ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2042/Main.java
@@ -0,0 +1,19 @@
+package cn.oriki.hdu.pid2042;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        while (n-- > 0) {
+            int a = in.nextInt();
+            int sum = 3;
+            while (a-- > 0) {
+                sum = (sum - 1) * 2;
+            }
+            System.out.println(sum);
+        }
+    }
+}
