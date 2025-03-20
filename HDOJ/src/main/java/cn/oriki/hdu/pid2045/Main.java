9d3df1bfef233b107f765679f4c1ac70b6dcb971
oriki <n0rk1n@live.cn>
2025-03-20 10:40:28 +0800

    HDU 2045. 不容易系列之 (3)—— LELE 的 RPG难 题 🤔️

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2045/Main.java
@@ -0,0 +1,25 @@
+package cn.oriki.hdu.pid2045;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static long[] x = new long[55];
+
+    static {
+        x[0] = 3;
+        x[1] = 6;
+        x[2] = 6;
+        for (int i = 3; i < 50; i++) {
+            x[i] = x[i - 1] + x[i - 2] * 2;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNextInt()) {
+            int n = in.nextInt();
+            System.out.println(x[n - 1]);
+        }
+    }
+}
