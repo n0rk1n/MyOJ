917734587d05b1de41c8e1d462be5427b9e00fd7
oriki <n0rk1n@live.cn>
2025-03-24 22:33:59 +0800

    HDU 2055. An easy problem ✅

---
+++ HDOJ/src/main/java/cn/oriki/hdu/pid2055/Main.java
@@ -0,0 +1,21 @@
+package cn.oriki.hdu.pid2055;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        while (t-- > 0) {
+            String x = in.next();
+            char[] c = x.toCharArray();
+            int y = in.nextInt();
+            if (Character.isUpperCase(c[0])) {
+                System.out.println(c[0] - 64 + y);
+            } else {
+                System.out.println(96 - c[0] + y);
+            }
+        }
+    }
+}
